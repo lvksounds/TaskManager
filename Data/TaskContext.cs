e7ade55a140a3c604a5eb6aea2a1e70c5200a2ef
Lucas <lvksounds@gmail.com>
2024-05-29 16:48:36 -0300

    initial commit

---
+++ Data/TaskContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TaskManaerAPI.Data
+{
+    public class TaskContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public TaskContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetConnectionString("MongoDB"));
+            _database = client.GetDatabase("TaskManagerDB");
+        }
+
+        public IMongoCollection<Task> Tasks => _database.GetCollection<Task>("Tarefas");
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManaerAPI.Models
+{
+    public class Task
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ConclusionDate { get; set; }
+        public string Status { get; set; }
+
+    }    
+}
